8abd7db4399a4c4a53639c08047fec672bc42614
Laust <laust.bonnesen@mail.com>
2023-01-06 01:41:55 +0100

    Initial commit

---
+++ src/main/java/laustrup/bandwichviewdebugging/BandwichViewDebuggingApplication.java
@@ -0,0 +1,13 @@
+package laustrup.bandwichviewdebugging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandwichViewDebuggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BandwichViewDebuggingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/laustrup/bandwichviewdebugging/BandwichViewDebuggingApplicationTests.java
@@ -0,0 +1,13 @@
+package laustrup.bandwichviewdebugging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandwichViewDebuggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
