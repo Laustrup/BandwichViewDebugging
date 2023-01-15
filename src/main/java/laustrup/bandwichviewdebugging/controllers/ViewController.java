65b3811b13e51e18cfa1db5f18ec950ca7db287e
Laust <laust.bonnesen@mail.com>
2023-01-15 19:27:44 +0100

    With allow origin

--- src/main/java/laustrup/bandwichviewdebugging/controllers/ViewController.java
+++ src/main/java/laustrup/bandwichviewdebugging/controllers/ViewController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @Controller
 public class ViewController {
 
